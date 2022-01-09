308fc2355b0a82fc9824cb70ae31cec164d3b691
Shubham Lad <shubhamlad1001@gmail.com>
2022-01-09 21:00:19 +0530

    basic api setup and mongodb connection

---
+++ backend/config/dbConnect.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const dbConnect = () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Successfully Connected to database 🚀"));
+};
+
+export default dbConnect;
